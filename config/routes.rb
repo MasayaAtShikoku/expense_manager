4fb752a16607d1f9e547ab2a6e26a19a21930312
Masaya Hori <masaya-hori@shikoku-kakoki.co.jp>
2018-11-30 06:56:38 +0000

    sort items on category show view

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :items
   end
 
-  root to: "items#index"
+  root to: "categories#index"
   resources :items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
