8d60a406c1a8640d70b7db7916489ab6bd41dedc
Masaya Hori <masaya-hori@shikoku-kakoki.co.jp>
2018-11-30 00:48:30 +0000

    sort items by name in category show view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.order(:description)
   end
 
   # GET /items/1
