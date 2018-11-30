00dcdfd7b4931da98737a071b4eb4ebc750f66dd
Masaya Hori <masaya-hori@shikoku-kakoki.co.jp>
2018-11-30 06:46:26 +0000

    add list button on item show view

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :periodicity
   belongs_to :category
+
+  def self.grand_annual_total_amount
+    Item.where(periodicity_id: 2).sum(:amount) + Item.where(periodicity_id: 1).sum(:amount) * 12
+  end
+
+  def self.grand_monthly_total_amount
+    Item.where(periodicity_id: 1).sum(:amount) + Item.where(periodicity_id: 2).sum(:amount) / 12
+  end
 end
