ba305c5bef5c41035efead30f7811563aab71d53
Masaya Hori <masaya-hori@shikoku-kakoki.co.jp>
2018-11-30 05:02:45 +0000

    change the layout of item show view

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,15 @@
 class Category < ApplicationRecord
   has_many :items
+
+  def total_amount
+    items.sum(:amount)
+  end
+
+  def annual_total_amount
+    items.where(periodicity_id: 2).sum(:amount) + items.where(periodicity_id: 1).sum(:amount) * 12
+  end
+
+  def monthly_total_amount
+    items.where(periodicity_id: 1).sum(:amount) + items.where(periodicity_id: 2).sum(:amount) / 12
+  end
 end
