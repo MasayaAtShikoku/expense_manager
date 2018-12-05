97281d79914c643545a02ff7a472ef99b9bb85f8
Masaya Hori <masaya-hori@shikoku-kakoki.co.jp>
2018-12-05 07:17:47 +0000

    added link to verndors on navv-bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_024912) do
+ActiveRecord::Schema.define(version: 2018_12_05_071258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2018_11_29_024912) do
   end
 
   create_table "periodicities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "vendors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
